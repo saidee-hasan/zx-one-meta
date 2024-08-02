dbb7811da5a7dd52b4db9c46f2fca59131a58a93
saideeWD <mdsaideehasan@gmail.com>
2024-08-02 09:56:27 +0600

    successfylly

--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -4,20 +4,37 @@ import { useParams } from "react-router-dom";
 
 const Room = () => {
   const { roomId } = useParams();
-  const newMeating = async (e) => {
+  const newMeeting = async (e) => {
     const appID = 541512934;
     const serverSecret = "bf92821021366f71fc638631d5b596e1";
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
       appID,
       serverSecret,
       roomId,
-      Date.now(),
+      Date.now().toString(),
       "Saidee Hasan"
     );
+
+    const zc =ZegoUIKitPrebuilt.create(kitToken);
+    zc.joinRoom({
+        container:e,
+        sharedLinks:[
+            {
+                name:'Copy Link',
+                url:`http://localhost:3000/room/${roomId}`
+            }
+        ],
+        scenario:{
+            mode : ZegoUIKitPrebuilt.GroupCall,
+            
+        },
+showScreenSharingButton:true,
+    })
   };
   return (
     <div>
-      <h1>Room {roomId}</h1>
+
+      <div ref={newMeeting} />
     </div>
   );
 };
