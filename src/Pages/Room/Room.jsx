cf533d7441ae4be6ac9e623470cf0dbceadbcb43
saideeWD <mdsaideehasan@gmail.com>
2024-08-02 09:37:38 +0600

    zncloud

--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Room = () => {
-    return (
-        <div>
-            Room
-        </div>
+  const { roomId } = useParams();
+  const newMeating = async (e) => {
+    const appID = 541512934;
+    const serverSecret = "bf92821021366f71fc638631d5b596e1";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now(),
+      "Saidee Hasan"
     );
+  };
+  return (
+    <div>
+      <h1>Room {roomId}</h1>
+    </div>
+  );
 };
 
 export default Room;
